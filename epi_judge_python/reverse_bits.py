f48b3ee3c7ba66476955f5291e2d3da086fbc5d8
raphobe <raphobe@users.noreply.github.com>
2020-05-11 04:47:48 +0000

    first shot at crummy solution

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
+class BitReverser:
+    def __init__(self):
+        pass
+
+    def sixty_four_bit_reverse(self, val):
+        constructed_val = 0
+        if self._is_odd(val):
+            constructed_val = constructed_val + 1
+
+        for i in range(0,63):
+            val = val >> 1
+            constructed_val = constructed_val << 1
+            if self._is_odd(val):
+                constructed_val = constructed_val + 1
+
+        return constructed_val
+
+    def _is_odd(self, val):
+        return val % 2 == 1
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    reverser = BitReverser()
+    return reverser.sixty_four_bit_reverse(x)
 
 
 if __name__ == '__main__':
